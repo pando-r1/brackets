f2007b5ed28fe091c134ad921350d7ea9f15e1f2
Anton <deftume@google.com>
2022-08-08 23:20:45 +0300

    feat: resolved task for current requirements

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  const openBrackets = [];
+  const closeBrackets = {};
+  const reverseCloseBrackets = {};
+
+  for (let bracket of bracketsConfig) {
+    openBrackets.push(bracket[0]);
+    closeBrackets[bracket[1]] = bracket[0];
+    reverseCloseBrackets[bracket[0]] = bracket[1];
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    if (stack.length > 0 && currentSymbol === stack[stack.length - 1] && 
+      currentSymbol === reverseCloseBrackets[currentSymbol]) {
+      stack.pop();}
+    else if (openBrackets.includes(currentSymbol)) {
+      stack.push(currentSymbol)
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      let topElement = stack[stack.length - 1]
+      if (topElement === closeBrackets[currentSymbol]) {
+        stack.pop();
+      } else return false
+    }
+  }
+  return stack.length === 0;
 }
